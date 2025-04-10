94b2e0b759e4bfb371904b4a85295e18944fafbe
Abhishek Khati <abhishekkhati39@gmail.com>
2025-04-11 00:53:59 +0545

    Refactor code structure for improved readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,37 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import localFont from "next/font/local";
+import { Solway } from "next/font/google";
 import Provider from "./Provider";
+
+const gaegu = localFont({
+  src: [
+    {
+      path: "../../public/fonts/gaegu/Gaegu-Light.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gaegu/Gaegu-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/gaegu/Gaegu-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-gaegu",
+  display: "swap",
+});
+
+const solway = Solway({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "800"],
+  variable: "--font-solway",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,11 +44,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link href="https://fonts.cdnfonts.com/css/gaegu" rel="stylesheet" />
-        <link href="https://fonts.cdnfonts.com/css/solway" rel="stylesheet" />
-      </head>
+    <html lang="en" className={`${gaegu.variable} ${solway.variable}`}>
       <body className={`antialiased`}>
         <Provider>{children}</Provider>
       </body>
